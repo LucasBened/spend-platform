7be534765744fa528bbf8a078dbf82c1886714f4
lucasbened <lussivbsilva1@gmail.com>
2024-11-02 20:06:40 -0300

    Criando projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text 2xl text-white font-bold bg-slate-950">OLa</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
